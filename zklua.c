339888c35c5fb33f4e5653c5bada638ed1f19476
forhappy <haipingf@gmail.com>
2013-04-04 23:58:54 +0800

    add pre-check function.

--- zklua.c
+++ zklua.c
@@ -197,134 +197,220 @@ static int zklua_close(lua_State *L)
 }
 
 static int zklua_client_id(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_recv_timeout(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_get_context(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set_context(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set_watcher(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_get_connected_host(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_interest(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_process(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_state(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_acreate(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_adelete(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_aexists(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_awexists(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 
 static int zklua_aget(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_awget(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_aset(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_aget_children(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_aget_children2(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_awget_children(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_awget_children2(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_async(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_aget_acl(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_aset_acl(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_error(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_add_auth(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_is_unrecoverable(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set_debug_level(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set_log_stream(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_deterministic_conn_order(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_create(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_delete(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_exists(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_wexists(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_get(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_wget(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set2(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_get_children(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_wget_children(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_get_children2(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_wget_children2(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_get_acl(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static int zklua_set_acl(lua_State *L)
-{}
+{
+    zklua_handle_t *handle = luaL_checkudata(L, 1, ZKLUA_METATABLE_NAME);
+}
 
 static const luaL_Reg zklua[] =
 {
