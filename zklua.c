e5e60ff4849816cba17da82c7ac731d76434cec9
forhappy <haipingf@gmail.com>
2013-04-13 10:20:30 +0800

    update LICENSE in *.h and *.c

--- zklua.c
+++ zklua.c
@@ -1,17 +1,22 @@
-/*
- * ========================================================================
+/* Zklua: Lua Binding Of Apache ZooKeeper
  *
- *       Filename:  zklua.c
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *    Description:  lua binding of apache zookeeper.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *        Created:  04/02/2013 05:17:01 PM
- *
- *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
- *        Company:  ICT ( Institute Of Computing Technology, CAS )
- *
- * ========================================================================
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -190,6 +195,7 @@ void acl_completion_dispatch(int rc, struct ACL_vector *acl,
 /**
  * return 1 if @host@ has the following format:
  * "127.0.0.1:2081,127.0.0.1:2082,127.0.0.1:2083".
+ * TODO: check if the given host is valid.
  **/
 static int _zklua_check_host(const char *host)
 {
